09d068b2896236e0e799588c6ffd0726c892ebb8
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-03-08 13:48:54 +0000

    Used arrow function for Task 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,12 +3,8 @@ import getBudgetObject from './7-getBudgetObject.js';
 export default function getFullBudgetObject(income, gdp, capita) {
   getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    getIncomeInDollars() {
-      return `$${income}`;
-    },
-    getIncomeInEuros() {
-      return `${income} euros`;
-    },
+    getIncomeInDollars: () => `$${income}`,
+    getIncomeInEuros: () => `${income} euros`,
   };
 
   return fullBudget;
