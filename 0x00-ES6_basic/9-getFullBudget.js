877daa2ef33f3872266ceded02740c07e244b85d
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-03-09 08:07:14 +0000

    Fixed issues with Task 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line import/extensions
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    getIncomeInDollars: () => `$${income}`,
-    getIncomeInEuros: () => `${income} euros`,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
