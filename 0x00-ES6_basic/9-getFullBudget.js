1d8edaea987e01858a9791d28f545352a170ad15
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-03-09 08:31:13 +0000

    Fixed Task 9 again

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -2,10 +2,13 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-  getBudgetObject(income, gdp, capita);
+  // eslint-disable-next-line no-unused-vars
+  const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuros: (income) => `${income} euros`,
+    // eslint-disable-next-line no-unused-vars
+    getIncomeInDollars: (budget) => `$${income}`,
+    // eslint-disable-next-line no-unused-vars
+    getIncomeInEuros: (budget) => `${income} euros`,
   };
 
   return fullBudget;
