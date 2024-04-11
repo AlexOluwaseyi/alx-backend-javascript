b5cdf0f1c82bf15e6024032b3c49264eb63d33d4
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-11 07:01:16 +0100

    101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,15 +1,8 @@
-export default function createIteratorObject(report) {
-  function* iterator() {
-    let result = ''; // Initialize an empty string to accumulate the values
-    /* eslint-disable guard-for-in */
-    for (const department in report.allEmployees) {
-      const employees = report.allEmployees[department];
-      for (const employee of employees) {
-        result += yield `${employee} | `; // Append each yielded value to the string
-      }
-    }
-    console.log(result); // Return the accumulated string when the generator is exhausted
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+  for (const employee of reportWithIterator) {
+    result += `${employee} | `;
   }
-
-  return iterator();
+  result = result.slice(0, -3);
+  return result;
 }
