a92a8b4d7329eb821f83e1c3b0c119664da90efb
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-11 07:05:20 +0100

    Promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+export default function getResponseFromAPI() {
+  const promise = new Promise((resolve, reject) => {
+    resolve();
+    reject();
+  });
+  return promise;
+}
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve("{ status: 200, body: 'Success' }");
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return promise;
+}
