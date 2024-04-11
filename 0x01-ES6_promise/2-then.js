c705688d34f09b03b8a4737718d7d0122463116a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-11 08:17:58 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,18 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error(''));
-    }
-  });
-}
-
-// const promise = true;
-handleResponseFromAPI()
-  .then((result) => result)
-  .catch((error) => error)
-  .finally(() => {
-    console.log('Got a response from the API');
-  });
+  return Promise.resolve({ status: 200, body: 'Success' })
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      return new Error('');
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+};
