d8eeda9b9091f590116bcc7c70aaa4556d1adb31
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-12 19:18:30 +0100

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,24 +1,14 @@
 /* eslint no-underscore-dangle: "off" */
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._name = name;
     this._length = length;
     this._students = Array.isArray(students) ? students : [];
-
-    if (typeof this._name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof this._length !== 'number' || this._length <= 0) {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(this._students)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    for (const student of this._students) {
-      if (typeof student !== 'string') {
-        throw new TypeError('Each student must be a string');
-      }
-    }
   }
 
   // getter
