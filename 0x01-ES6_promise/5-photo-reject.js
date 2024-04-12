577896c7e1f0b5ea44349d9bd9161cf74ef2c90d
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-12 06:18:16 +0100

    uncaught error

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,4 @@
 export default function uploadPhoto(filename) {
-  // eslint-disable-next-line no-unused-vars
-  return new Promise((resolve, reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  }).catch((error) => console.log(error));
+  return Promise.reject(new Error(`${filename} cannot be processed`))
+  // .catch((error) => console.log(error));
 }
