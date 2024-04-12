d53fdef1a897ac7de6d17c0086165943c590f690
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-12 10:14:29 +0100

    Classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,11 +1,6 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this.maxStudentsSize = maxStudentsSize;
-  }
-
-  // Getter
-  // eslint-disable-next-line no-underscore-dangle
-  get _maxStudentsSize() {
-    return this.maxStudentsSize;
+    // eslint-disable-next-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
