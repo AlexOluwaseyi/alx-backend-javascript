01d5729abd9063de7a282efaf4a89e985791f755
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-13 06:59:39 +0100

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,30 @@
+/* eslint no-underscore-dangle: "off" */
+export default class Building {
+  constructor(sqft) {
+    if (typeof (sqft) === 'number') {
+      this._sqft = sqft;
+    } else {
+      throw new TypeError('SQFT must be a number');
+    }
+    if (this.constructor.name !== 'Building'){
+      if (this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  // Setter
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  // Getter
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Evacuation method for abstract class
+  evacuationWarningMessage() { // eslint-disable-line class-methods-use-this
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
