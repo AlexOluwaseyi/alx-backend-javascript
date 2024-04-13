f4423747c373be149f84c6cb237265fbff8f0fa8
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-14 00:26:12 +0100

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -9,7 +9,7 @@ export default function getFullResponseFromAPI(success) {
   });
 }
 
-const success = false; 
+const success = false;
 getFullResponseFromAPI(success)
   .then((result) => {
     console.log(result);
