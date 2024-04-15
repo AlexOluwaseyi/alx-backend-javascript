e04c187a5fc42af0e75da092bec5380b26f85403
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-15 16:02:47 +0100

    9-try.js

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const response = mathFunction();
+    queue.push(response);
+  } catch (error) {
+    queue.push(`${error.name}: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
