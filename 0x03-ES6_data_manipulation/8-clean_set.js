95c19a54a79c398f2d22b4edc9314b8f3d3b2306
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-17 13:28:52 +0100

    8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (startString.length === 0)
+    return ''
+  let str = '';
+  const arr =  Array.from(set);
+  for (const value of arr) {
+    if (value.startsWith(startString)) {
+      const temp = value.split(startString);
+      str += temp[1];
+      str += '-';
+    }
+  }
+  const finalStr = str.slice(0, -1);
+  return finalStr;
+}
