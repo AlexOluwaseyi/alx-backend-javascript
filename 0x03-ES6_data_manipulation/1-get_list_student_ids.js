f7cf017b5899f20588054336bba798ff2e048453
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-17 07:54:38 +0100

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,11 @@
 export default function getListStudentIds(studentList) {
-  //const studentId = [];
-  if (!Array.isArray(studentList)){
-    return studentId;
+  // const studentId = [];
+  if (!Array.isArray(studentList)) {
+    return [];
   }
-  //for (let i = 0; i < studentList.length; i++){
+  // for (let i = 0; i < studentList.length; i++){
   //  studentId.push(studentList[i]['id']);
-  //}
-  //return studentId;
-  return studentList.map(student => student.id);
+  // }
+  // return studentId;
+  return studentList.map((student) => student.id);
 }
