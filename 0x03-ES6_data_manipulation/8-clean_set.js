3e7427e1f262220fab0cb4d8299909bf4a567796
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-20 09:24:49 +0100

    resolved test cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) return '';
+  if (startString !== undefined && startString.length === 0) return '';
   let str = '';
   const arr = Array.from(set);
   for (const value of arr) {
-    if (value.startsWith(startString)) {
-      const temp = value.split(startString);
-      str += temp[1];
+    if (value !== undefined && value.startsWith(startString)) {
+      str += value.replace(startString, '');
+      // const temp = value.split(startString, 2);
+      // str += temp[1];
       str += '-';
     }
   }
