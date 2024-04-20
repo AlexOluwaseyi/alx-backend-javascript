7f8e8634f13bf17d570be21409a3ebdfa9653e8c
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-04-20 05:03:02 +0100

    task_4

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,17 @@
+namespace Subjects {
+  export interface Teacher {
+      experienceTeachingJava?: Number;
+    }
+  export class Java extends Subject {
+    getRequirements(): string{
+      return "Here is the list of requirements for Java";
+    }
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava !== undefined) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
+    }
+  }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,17 @@
+namespace Subjects {
+  export interface Teacher {
+      experienceTeachingReact?: Number;
+    }
+  export class React extends Subject {
+    getRequirements(): string{
+      return "Here is the list of requirements for React";
+    }
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingReact !== undefined) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
+    }
+  }
+}
