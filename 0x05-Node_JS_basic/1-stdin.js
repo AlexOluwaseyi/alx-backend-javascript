715cebe4e93e1c3251a303a671cf5d13337de351
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-10 02:54:57 +0100

    eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,11 @@ function stdin() {
   process.stdout.write('Welcome to Holberton School, what is your name?\n');
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
-    // console.log('This important software is now closing');
-    // process.exit();
   });
   process.stdin.on('end', () => {
     process.stdout.write('This important software is now closing\n');
-    process.exit()
+    process.exit();
   });
-  // process.exit()
 }
 
 stdin();
